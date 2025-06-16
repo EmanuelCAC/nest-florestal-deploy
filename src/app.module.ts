18129698c8e7d1eccaa9161387efdf3ffb6ef7db
EmanuelCAC <emanuelc279@gmail.com>
2025-06-16 09:30:25 -0300

    add autoInfracao to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,21 @@ import { PrismaModule } from './prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { AutoInfracaoModule } from './autoInfracao/autoInfracao.module';
+import { AutoInfracaoController } from './autoInfracao/autoInfracao.controller';
+import { AutoInfracaoService } from './autoInfracao/autoInfracao.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, JwtModule.register({
-    secret: 'seu-segredo',
-    signOptions: { expiresIn: '60s' },
-  })],
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [
+    AuthModule,
+    PrismaModule,
+    AutoInfracaoModule,
+    JwtModule.register({
+      secret: 'seu-segredo',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [AuthController, AutoInfracaoController],
+  providers: [AuthService, AutoInfracaoService],
 })
 export class AppModule {}
